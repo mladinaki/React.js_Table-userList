e0319c06de097956d83a04398fe4620a170015f6
Mladen <mladinaki54@gmail.com>
2024-07-14 18:39:39 +0300

    add to first commit

---
+++ client/src/components/UserList.jsx
@@ -0,0 +1,14 @@
+import Search from "./Search";
+import UserListTable from "./UserListTable";
+
+const UserList = () => {
+    return (
+        <section className="card users-container">
+          
+            
+            <UserListTable />
+        </section>
+    );
+};
+
+export default UserList;
